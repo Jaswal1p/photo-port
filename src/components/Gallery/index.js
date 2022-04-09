aca0f542a4dcab4f39975d957c842f5547f51d39
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-08 20:12:53 -0400

    created galley component and connected to iages

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg";
 
-function Gallery() {
+function Gallery(props) {
+
+    const currentCategory ={
+        name: "commercial",
+        description: 
+          "Photos of grocery stores, food trucks, and other commercial projects",
+    };
 
     return (
         <section>
 
-            <h1>NAME</h1>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
 
-            <p>DESCRIPTION</p>
+            <p>{currentCategory.name}</p>
+
+            <div>
+                <img src={photo} 
+                
+                alt="Commercial Example" 
+                
+                className="img-thumbnail mx-1"
+                
+                />
+
+            </div>
 
         </section>
     );
