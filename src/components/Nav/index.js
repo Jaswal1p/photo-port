1329d9716c3ea9eccd6c0f082104b471050e3245
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-08 22:21:50 -0400

    added condtional rendering of Nav and added useeffect for browser tab

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,50 +1,93 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 
-const Nav = () =>{
+// useEffect(() => {
+//     document.title = capitalizeFirstLetter(currentCategory.name);
+// }, [currentCategory]);
 
-    const  categories = [
-        { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
-        { name: 'portraits', description: 'Portraits of people in my life' },
-        { name: 'food', description: 'Delicious delicacies' },
-        { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
-      ];
+
+// The following commented out code was 'lifted' to the paremt component App.js & props function  added
+
+// const Nav = () =>{
+
+//     const  [categories] = useState([
+//         { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
+//         { name: 'portraits', description: 'Portraits of people in my life' },
+//         { name: 'food', description: 'Delicious delicacies' },
+//         { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
+//       ]);
+
+//       const [currentCategory, setCurrentCategory] = useState(categories[0]);
     
-      const handleClick = () => {
-        console.log("click handled")
-      }
+    //   const handleClick = () => {
+    //     console.log("click handled")
+    //   }
 
-    return(
+function Nav(props) {
+
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, );
+
+        const { 
+            categories = [],
+
+            setCurrentCategory,
+
+            currentCategory,
+        } = props;
+    
+
+       return(
         <header className="flex-row px-1">
             <h2>
                 <a data-testid="link" href="/">
-                <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                    <span role="img" aria-label="camera"> 
+                    {" "}
+                    📸
+                    </span>{" "}
+                    Oh Snap!
+
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
+                    
                     <li className="mx-2">
-                        <a data-testid="about" href="#about" onClick={() => handleClick()}>
+                        
+                        <a 
+                          href="#about" 
+                        >
                         About me
                         </a>
+
                     </li>
-                    <li className={"mx-2"}>
-                        <span onClick={() => handleClick()}>
-                            Contact
-                            </span>
+
+                    <li>
+                        <span>Contact</span>
                     </li>
+                    
                     {categories.map((category) => (
-                        <li
-                        className="mx-1"
-                        key={category.name}
-                        >
-                        <span onClick={() => { handleClick(); }}>
-                            {capitalizeFirstLetter(category.name)}
-                            
+
+                        <li className={`mx-1 ${
+                            currentCategory.name === category.name && 'navActive'
+                        }`} key={category.name}>
+
+                        <span
+                        
+                          onClick={() => {
+                            setCurrentCategory(category)
+                    }}
+                    >
+                        {capitalizeFirstLetter(category.name)}
                         </span>
+
                         </li>
                     ))}
+                   
+                    
                 </ul>
             </nav>
        </header>
