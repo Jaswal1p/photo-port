d8ffad30db32d06fbea586be45d6bf7b78c299a4
Jaswal1p <Jaswal1p@yahoo.com>
2022-04-10 01:19:34 -0400

    updated the index.js with createRoot API for React18 & state control for contactform(controlled component)

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+
+    const [formState, setFormState] = useState( { name: '', email: '', message: ''});
+
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    console.log(formState);
 
     // JSX
 
@@ -13,17 +23,17 @@ function ContactForm() {
 
             <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" name="name" />
+                <input type="text" name="name" onChange= {handleChange}defaultValue={name}/>
             </div>
 
             <div>
                 <label htmlFor="email">Email address:</label>
-                <input type="email" name="email" />
+                <input type="email" name="email" onChange= {handleChange} defaultValue={email}/>
             </div>
 
             <div>
                 <label htmlFor="message">Message:</label>
-                <textarea name="message" rows="5"  />
+                <textarea name="message" rows="5" onChange= {handleChange} defaultValue={message} />
             </div>
 
             <button type="submit">Submit</button>
